53b7870047eff03424d13d3b72fc8e880c4f7f6f
David Young <sporty-iron883@protonmail.com>
2024-09-07 13:48:27 -0400

    Insults now working

--- src/main.lisp
+++ src/main.lisp
@@ -13,11 +13,20 @@
           while line
           collect (uiop:split-string line))))
 
-(defun generate-insults (&key (phrases-file default-phrases-file))
-  (let ((*phrases-path* phrases-file))
-    (collect-phrases)))
+(defun generate-insults (&key (phrases-file default-phrases-file) (count 500))
+  (let* ((*phrases-path* phrases-file)
+         (phrases (collect-phrases))
+         (nphrases (length phrases)))
+    (loop for i from 0 to (1- count)
+          collect (format nil "Thou ~d ~d ~d!"
+                          (first (nth (random nphrases) phrases))
+                          (second (nth (random nphrases) phrases))
+                          (third (nth (random nphrases) phrases))))))
 
-(defun insult-me ()
-  (let ((phrase (first (generate-insults))))
-    (format t "Thou ~d ~d ~d!" (first phrase) (second phrase) (third phrase))
+(defun insult-me (&key (count 1))
+  (let* ((insults (generate-insults))
+         (len (length insults)))
+    (loop for i from 0 to (1- count)
+          do
+             (format t "~d~%" (nth (random len) insults)))
     t))
